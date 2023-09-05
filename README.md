3074adcd19c7df80dfb547ccc54950200e5affc5
Anton <antongorynya1@gmail.com>
2023-09-05 17:43:01 +0300

    add DialogFlow Support

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # speech_recognition_bot
+
+# Установка
+## Получение credentials.json от Google-аккаунта
+Установите консольную утилиту [gcloud](https://cloud.google.com/sdk/docs/install). Перезапустите компьютер.
+После чего введите команду
+
+```commandline
+gcloud init 
+```
+Выполните аутентификацию в браузере.
+После чего выберете ваш проект
+```commandline
+Pick cloud project to use:  
+ [1] possible-symbol-380617 
+ [2] quickstart-1571052923647
+ [3] test-sheets-api-1571052007228
+ [4] tg-bot-rgfd
+ [5] Enter a project ID
+ [6] Create a new project
+Please enter numeric choice or text value (must exactly match list item):  4 
+
+```
+
+Для генрации credentials.json выполните
+```commandline
+gcloud auth application-default login
+```
+
+## Создание .env файла
+
+Заполните переменную окружения `GOOGLE_APPLICATION_CREDENTIALS`, где лежит путь до файла с ключами от Google.
+А так же `TG_TOKEN`, где лежит токен он вашего бота
--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 from environs import Env
+from google.cloud import dialogflow
+from google.cloud import api_keys_v2
+from google.cloud import language_v1
+from google.cloud.api_keys_v2 import Key
 from telegram.ext import Updater, ConversationHandler, CommandHandler, MessageHandler, Filters
+
+
+def detect_intent_text(project_id, session_id, text, language_code='ru-RU'):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    print("Query text: {}".format(response.query_result.query_text))
+    print(
+        "Detected intent: {} (confidence: {})\n".format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence,
+        )
+    )
+    print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
+
+    return response
 
 
 def start(update, _):
@@ -25,12 +52,17 @@ if __name__ == '__main__':
     env = Env()
     env.read_env()
     bot_token = env('TG_TOKEN')
+    google_key = env('GOOGLE_KEY')
+    project_id = env('PROJECT_ID')
 
-    updater = Updater(token=bot_token, use_context=True)
-    dispatcher = updater.dispatcher
+    detect_intent_text(project_id, 1, 'Привет')
 
-    dispatcher.add_handler(CommandHandler('cancel', cancel))
-    dispatcher.add_handler(CommandHandler('start', start, run_async=True))
-    dispatcher.add_handler(MessageHandler(Filters.text, echo))
-    updater.start_polling()
-    updater.idle()
+
+    # updater = Updater(token=bot_token, use_context=True)
+    # dispatcher = updater.dispatcher
+    #
+    # dispatcher.add_handler(CommandHandler('cancel', cancel))
+    # dispatcher.add_handler(CommandHandler('start', start, run_async=True))
+    # dispatcher.add_handler(MessageHandler(Filters.text, echo))
+    # updater.start_polling()
+    # updater.idle()
