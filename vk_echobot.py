1d4fa07da71dd25411c716ed1535884a76966263
Anton <antongorynya1@gmail.com>
2023-09-07 16:07:51 +0300

    add DialogFlow to VK bot.

--- vk_echobot.py
+++ vk_echobot.py
@@ -3,12 +3,15 @@ from environs import Env
 
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
+from dialog_flow_common import  detect_intent_text
 
 
-def echo(event, vk_api):
+def echo(event, vk_api, project_id):
+    response = detect_intent_text(project_id, event.user_id, event.text)
+
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
+        message=response.query_result.fulfillment_text,
         random_id=random.randint(1,1000)
     )
 
@@ -17,9 +20,12 @@ if __name__ == "__main__":
     env = Env()
     env.read_env()
     bot_token = env('VK_TOKEN')
+    google_key = env('GOOGLE_KEY')
+    project_id = env('PROJECT_ID')
+
     vk_session = vk.VkApi(token=bot_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
+            echo(event, vk_api, project_id)
